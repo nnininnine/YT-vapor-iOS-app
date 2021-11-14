4a548e756518d079e110a9b7aadd82c85945753b
Nuttapon Buaban <nuttapon.dev@gmail.com>
2021-11-14 21:32:35 +0700

    Initial Commit

---
+++ YT-vapor-iOS-app/YT_vapor_iOS_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  YT_vapor_iOS_appApp.swift
+//  YT-vapor-iOS-app
+//
+//  Created by Nuttapon Buaban on 14/11/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct YT_vapor_iOS_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
